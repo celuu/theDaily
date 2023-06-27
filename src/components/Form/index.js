ff1d2d61b8264db79c8e560d3a76baaa520fad18
celuu <christine.e.luu@gmail.com>
2023-06-27 15:59:34 -0700

    Add Chakra and react calendar

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
 
 
 const Form = () => {
-    const [date, setDate] = useState(Date.now.toString());
-    return(
-    <form>
-        <div>{date}</div>
-        <input></input>
-    </form>
-    )
+    const [date, setDate] = useState(new Date().toDateString());
+    return (
+      <form>
+        <div className="top-bar">
+          <IconButton aria-label="Search database" icon={<ArrowLeftIcon />} />
+          <div className="item-in-top-bar">{date}</div>
+          <IconButton aria-label="Search database" icon={<ArrowRightIcon />} />
+        </div>
+        <label>
+          <input></input>
+        </label>
+      </form>
+    );
 }
 
 export default Form;
